292abea5eaa15db6f4b9ed90857afacf2a811994
JLRP09102005 <jlrp09102005@gmail.com>
2025-09-25 19:29:14 +0200

    Deleted script content to make from zero

--- Calculator/calculator_script.sh
+++ Calculator/calculator_script.sh
@@ -5,18 +5,3 @@ pregunta2="Primer número?"
 pregunta3="Segundo número?"
 resultado=0
 opcion=-1
-
-echo "$pregunta1"
-read opcion
-
-echo "$pregunta2"
-read num1
-
-echo "$pregunta3"
-read num2
-
-if [ $pregunta1 -eq 1]
-then
-	resultado = $((pregunta1 + pregunta2))
-	echo "$resultado"
-fi
