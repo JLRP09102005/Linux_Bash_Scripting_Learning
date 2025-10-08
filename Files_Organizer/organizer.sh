c7bc50a0ef3cb03f426d4f1c525e972be294296c
Jose Luis Ruiz Perez <jlrp09102005@gmail.com>
2025-10-08 10:59:52 +0200

    fixing script

--- Files_Organizer/organizer.sh
+++ Files_Organizer/organizer.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-filesPath = @1
+#GET NECESSARY DIRECTORIES
+filesPath = "$1"
 currentPath = $(pwd)
+
+#CREATE ORGANIZER FOLDERS PATH
 textDirectory = "$currentPath/TextFiles"
+imagesDirectory = "$currentPath/ImageDirectory"
+videosDirectory = "$currentPath/VideoDirectory"
 
+#CREATE ORGANIZER FOLDERS
 mkdir -p "$textDirectory"
+mkdir -p "$imagesDirectory"
+mkdir -p "$videosDirectory"
 
+#MOVE ARCHIVES
 mv "$filesPath"/*.txt "$textDirectory"
+
+mv "filesPath"/*.png "imagesDirectory"
+mv "filesPath"/*.jpg "imagesDirectory"
+
+mv "filesPath"/*.mp4 "videosDirectory"
+mv "filesPath"/*.mkv "videosDirectory"
