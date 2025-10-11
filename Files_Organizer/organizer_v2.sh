33479b644e6ea0b27b1e2de7dfeb02308e411593
Jose Luis Ruiz Perez <jlrp09102005@gmail.com>
2025-10-11 19:40:57 +0200

    Finished organizer v2

--- Files_Organizer/organizer_v2.sh
+++ Files_Organizer/organizer_v2.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-filesPath="$1"
-currentPath=$(pwd)
+if [ "$1" == "help" ]
+then
+    echo ""
 
-echo "Write the extensions to organize (separated by ",")"
-echo "Example: .txt,.mp4,.mp3"
-read -p "> " input
+    echo ">SINTAXI: organizer_v2 [files path]"
 
-IFS=',' read -ra extensions <<< "$input"
+    echo ""
 
-echo ""
-echo "Organizando archivos..."
-echo ""
+    echo ">ABOUT: this script organizes the files from a general directory on separated directories my the extensions indicated"
+else
+    filesPath="$1"
+    currentPath=$(pwd)
 
-echo "${extensions[0]}"
+    echo "Write the extensions to organize (separated by ",")"
+    echo "Example: .txt,.mp4,.mp3"
+    read -p "> " input
+
+    IFS=',' read -ra extensions <<< "$input"
+
+    echo ""
+    echo "Organizando archivos..."
+    echo ""
+
+    for ext in "${extensions[@]}"
+    do
+        ext=$(echo "$ext" | tr -d ' ' | tr -d '.')
+
+        extDirectory="$currentPath/${ext}_files"
+        mkdir -p "$extDirectory"
+
+        if ls "$filesPath"/*."${ext}" 1> /dev/null 2> /dev/null
+        then
+            mv "$filesPath"/*."${ext}" "$extDirectory"
+        fi
+    done
+fi
