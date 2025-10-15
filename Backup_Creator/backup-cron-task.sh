239a433ebd19ea0534bf917fbdb174e79221d1ab
Jose Luis Ruiz Perez <jlrp09102005@gmail.com>
2025-10-15 22:21:39 +0200

    save

--- Backup_Creator/backup-cron-task.sh
+++ Backup_Creator/backup-cron-task.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+backupsPath="~/Backups"
 
 if [ $# -lt 1 ]
 then
     echo "Backup cron task need at least 1 argument"
 else
-    directoryPath="$1"
-    mkdir -p "$directoryPath"
+    directories="$1"
+    IFS=',' read -ra directoriesPath <<< "$directories"
+
+    for directory in "${directoriesPath[@]}"
+    do
+        mkdir -p "$backupsPath"
+
+        if [ -e "$directory" ]
+        then
+            cp -r "$directory" "$backupsPath"
+        else
+            echo "backup-cron-task couldn't find backup directory"
+        fi
+    done
 fi
--- Backup_Creator/backup-interface.sh
+++ Backup_Creator/backup-interface.sh
@@ -5,15 +5,18 @@ then
     echo ""
     echo "Script Usage:"
     echo ""
-
-    echo "**At time select use '*' to select all options, use '/' to set an interval of time (5/7), use ',' to set multiple values, use '-' to set a range of times (1-5). Use the same time sistem of cron shell command"
+    echo "At time select use '*' to select all options, use '/' to set an interval"
+    echo "of time (*/5), use ',' to set multiple values, use '-' to set a range (1-5)."
+    echo "Use the same time system as cron shell command."
     echo ""
-
-    echo "--Arg 1: minutes"
-    echo "--Arg 2: hours"
-    echo "--Arg 3: month day"
-    echo "--Arg 4: month"
-    echo "--Arg 5: day of week"
+    echo "Arguments:"
+    echo "  Arg 1: minutes (0-59)"
+    echo "  Arg 2: hours (0-23)"
+    echo "  Arg 3: month day (1-31)"
+    echo "  Arg 4: month (1-12)"
+    echo "  Arg 5: day of week (0-7, where 0 and 7 are Sunday)"
+    echo ""
+    echo "Example: ./script.sh 30 2 * * * "
     echo ""
 else
     if [ $# -lt 5 ]
@@ -25,16 +28,14 @@ else
         monthDays=$3
         months=$4
         weekDays=$5
+        scriptPath="/home/jozelu/Desktop/GitHub/Linux_Bash_Scripting_Learning/Backup_Creator/backup-cron-task.sh"
 
         echo "Write the directories path to back up"
         echo "**Example: Desktop/Github,home/bin"
         read -p ">  " directories
 
-        IFS=',' read -ra directoriesPath <<< "$directories"
-
-        for directory in "${directoriesPath[@]}"
-        do
-            echo "$directory"
-        done
+        "$directories" | tr -d ' '
+        crontabString="$minutes $hours $monthDays $months $weekDays bash $scriptPath \"$directories\""
+        (sudo crontab -l 2>/dev/null; echo "$crontabString") | sudo crontab -
     fi
 fi
