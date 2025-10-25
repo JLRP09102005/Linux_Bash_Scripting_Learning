129eff7ae7adfd89a1e0ebe6d2ddc4fea0c50e4c
Jose Luis Ruiz Perez <jlrp09102005@gmail.com>
2025-10-25 17:19:22 +0200

    finished basic script

--- Backup_Creator/backup-interface.sh
+++ Backup_Creator/backup-interface.sh
@@ -16,19 +16,19 @@ then
     echo "  Arg 4: month (1-12)"
     echo "  Arg 5: day of week (0-7, where 0 and 7 are Sunday)"
     echo ""
-    echo "Example: ./script.sh 30 2 * * * "
+    echo "Example: ./script.sh '30' '2' '*' '*' '*' "
     echo ""
 else
     if [ $# -lt 5 ]
     then
         echo "The script needs at least 5 arguments to work"
     else
-        minutes=$1
-        hours=$2
-        monthDays=$3
-        months=$4
-        weekDays=$5
-        scriptPath="/home/jozelu/Desktop/GitHub/Linux_Bash_Scripting_Learning/Backup_Creator/backup-cron-task.sh"
+        minutes="$1"
+        hours="$2"
+        monthDays="$3"
+        months="$4"
+        weekDays="$5"
+        scriptPath="$(pwd)/backup-cron-task.sh"
 
         echo "Write the directories path to back up"
         echo "**Example: Desktop/Github,home/bin"
