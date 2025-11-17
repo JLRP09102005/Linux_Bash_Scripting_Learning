1e248ee1407a896abf85d463cc39950b0123aec9
Jose Luis <jlrp09102005@gmail.com>
2025-11-17 23:46:21 +0100

    Save install script

--- Task_Notifier/Modular_Notifier/install.sh
+++ Task_Notifier/Modular_Notifier/install.sh
@@ -3,15 +3,61 @@
 #################################################
 ## MODULAR BLOCKS
 #################################################
+unit_section="[Unit]"
+service_section="[Service]"
+timer_section="[Timer]"
+install_section="[Install]"
+
+description="Description="
+type="Type="
+exec_start="ExecStart="
+restart="Restart="
+on_boot_sec="OnBootSec="
+on_unit_active_sec="OnUnitActiveSec="
+wanted_by="WantedBy="
 
 ########################################
 ## VARIABLES
 ########################################
+isRootUser="false"
+checker_boolean=""
 
 ######################################
 ## FUNCTIONS
 ######################################
+ErrorExit()
+{
+    echo "error"
+}
+
+CheckRootPrivilegies()
+{
+    if [ "$(whoami)" == "root" ]; then
+        isRootUser="true"
+    else
+        isRootUser="false"
+    fi
+}
+
+CheckOnlyNumbers()
+{
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
 
 ##############################################
 ## MAIN SCRIPT
 ##############################################
+CheckRootPrivilegies
+
+echo "Select service type to create (write the option number)"
+echo "  1.-Notification by timer"
+read -pr "> " serviceOption
+
+checker_boolean="$(CheckOnlyNumbers "$serviceOption")"
+if [ "$checker_boolean" == "false" ]; then
+    ErrorExit
+fi
